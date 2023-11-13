d778a60bbb40c531623cad9218964b0eacda088c
Akatsuki <handuy099@gmail.com>
2023-11-13 13:05:08 +0700

    add Material

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import './App.css'
 import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import { pink } from '@mui/material/colors'
+import HomeIcon from '@mui/icons-material/Home'
 
 function App() {
   return (
     <>
-      <Button variant='outlined'>Hello World</Button>
+      <div>Hello, world!</div>
+      <Button variant='text'>Text</Button>
+      <Button variant='contained'>Contained</Button>
+      <Button variant='outlined'>Outlined</Button>
+
+      <br />
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <HomeIcon />
+      <HomeIcon color='primary' />
+      <HomeIcon color='secondary' />
+      <HomeIcon color='success' />
+      <HomeIcon color='action' />
+      <HomeIcon color='disabled' />
+      <HomeIcon sx={{ color: pink[100] }} />
     </>
   )
 }
